3b1bc81dddb5d6e72e8d5826f6f3b70ef18ff682
LarissaUFMS <89803958+LarissaUFMS@users.noreply.github.com>
2022-05-18 15:21:56 -0400

    Update scanner.cpp

--- scanner.cpp
+++ scanner.cpp
@@ -7,8 +7,8 @@
 Scanner::Scanner(string input/*, SymbolTable* table*/)
 {
     this->input = input;
-    cout << "Entrada: " << input << endl << "Tamanho: " 
-         << input.length() << endl;
+    cout << "Entrada: " << input << endl << "Tamanho: "
+        << input.length() << endl;
     pos = 0;
     line = 1;
 
@@ -25,7 +25,7 @@ int Scanner::getLine()
 }
 
 //Método que retorna o próximo token da entrada
-Token* 
+Token*
 Scanner::nextToken()
 {
     Token* tok;
@@ -34,170 +34,193 @@ Scanner::nextToken()
 
     while (true)
     {
-        while (input[pos] == ' ')
+        while (isspace(input[pos]))
             pos++;
-        switch(state)
+        switch (state)
         {
-                case 1:
-                if (input[pos] == '<')
-                {
-                    pos++;
-                    state = 2;
-                }
-                else if (input[pos] == '=')
-                {
-                    pos++;
-                    state = 3;
-                }
-                else if (input[pos] == '>')
-                {
-                    pos++;
-                    state = 4;
-                }
-                else if (input[pos] == '!')
-                {
-                    pos++; 
-                    state = 5;
-                }
-                else if (input[pos] == '+')
-                {
-                    pos++;
-                    tok = new Token(OP, ADDITION);
-                    return tok;
-                }
-                else if(input[pos] == '-')
-                {
-                    pos++;
-                    tok = new Token(OP, SUBTRACTION);
-                    return tok;
-                }
-                else if(input[pos] == '*')
-                {
-                    pos++;
-                    tok = new Token(OP, ASTERISK);
-                    return tok;
-                }
-                else if(input[pos] == '/')
-                {
-                    pos++;
-                    tok = new Token(OP, BAR);
-                    return tok;
-                }
-                else if(input[pos] == '%')
-                {
-                    pos++;
-                    tok = new Token(OP, MODULAR);
-                    return tok;
-                }
-                else if (input[pos] == ';') {
-                    pos++;
-                    tok = new Token(SEP, SEMICOLON);
-                    return tok;
-                }
-                else if (input[pos] == '(') {
-                    pos++;
-                    tok = new Token(SEP, LEFT_PARENTHESE);
-                    return tok;
-                }
-                else if (input[pos] == ')') {
-                    pos++;
-                    tok = new Token(SEP, RIGHT_PARENTHESE);
-                    return tok;
-                }
-                else if (input[pos] == '{') {
-                    pos++;
-                    tok = new Token(SEP, LEFT_BRACE);
-                    return tok;
-                }
-                else if (input[pos] == '}') {
-                    pos++;
-                    tok = new Token(SEP, RIGHT_BRACE);
-                    return tok;
-                }
-                else if (input[pos] == ']') {
-                    pos++;
-                    tok = new Token(SEP, RIGHT_BRACE);
-                    return tok;
-                }
-                else if (input[pos] == '[') {
-                    pos++;
-                    tok = new Token(SEP, LEFT_BRACKET);
-                    return tok;
-                }
-                else if (input[pos] == ',') {
-                    pos++;
-                    tok = new Token(SEP, COMMA_LITERAL);
-                    return tok;
-                }
-                else if (input[pos] == ':') {
-                    pos++;
-                    tok = new Token(SEP, COLON);
-                    return tok;
-                }
-                else if (input[pos] == '.') {
-                    pos++;
-                    tok = new Token(SEP, DOT);
-                    return tok;
-                }
-                else if (isdigit(input[pos])) {
-                    pos++;
-                    state = 6;
-                }
-                break;
-
-            case 2:
-                if (input[pos] == '='){
+        case 1:
+            //Op < ou <=
+            if (input[pos] == '<')
+            {
+                pos++;
+                if (input[pos] == '=') {
                     pos++;
                     tok = new Token(OP, LESS_OR_EQUAL);
-                    return tok;
                 }
-                else{
+                else {
                     tok = new Token(OP, LESS_THAN);
-                    return tok;
                 }
-                break;
-
-            case 3:
-                if (input[pos] == '='){
+            }
+            //Op = ou ==
+            else if (input[pos] == '=')
+            {
+                pos++;
+                if (input[pos] == '=') {
                     pos++;
                     tok = new Token(OP, EQUAL);
-                    return tok;
                 }
-                else{
+                else {
                     tok = new Token(OP, ASSIGNMENT);
-                    return tok;
                 }
-                break;
-            case 4:
-                if (input[pos] == '='){
+            }
+            //Op >= ou >
+            else if (input[pos] == '>')
+            {
+                pos++;
+                if (input[pos] == '=') {
                     pos++;
                     tok = new Token(OP, GREATER_OR_EQUAL);
-                    return tok;
                 }
-                else{
+                else {
                     tok = new Token(OP, GREATER_THAN);
-                    return tok;
                 }
-                break;
-            case 5:
-                if (input[pos] == '='){
+            }
+            // Op que ! seguido de =,
+            // se o ! não for seguido de =
+            // ocorreu um erro léxico
+            else if (input[pos] == '!')
+            {
+                pos++;
+                if (input[pos] == '=') {
                     pos++;
                     tok = new Token(OP, NOT_EQUAL);
-                    return tok;
                 }
-                break;
-            case 6:
+                else {
+                    lexicalError("Expected = after !");
+                }
+            }
+            //Op +
+            else if (input[pos] == '+')
+            {
+                pos++;
+                tok = new Token(OP, ADDITION);
+            }
+            //Op -
+            else if (input[pos] == '-')
+            {
+                pos++;
+                tok = new Token(OP, SUBTRACTION);
+            }
+            //Op *
+            else if (input[pos] == '*')
+            {
+                pos++;
+                tok = new Token(OP, ASTERISK);
+            }
+            //Op / ou comentário
+            else if (input[pos] == '/')
+            {
+                pos++;
+                // comentário de linha
+                if (input[pos] == '/') {
+                    while (input[pos] != '\n')
+                        pos++;
+                    state = 1;
+                    //ignora o comentário
+                    //procura o próximo token
+                }
+                // comentário de bloco
+                else if (input[pos] == '*') {
+                    state = 2;
+                }
+                // Op /
+                else {
+                    tok = new Token(OP, BAR);
+                }
+            }
+            // Op %
+            else if (input[pos] == '%')
+            {
+                pos++;
+                tok = new Token(OP, MODULAR);
+            }
+            // Sep ;
+            else if (input[pos] == ';') {
+                pos++;
+                tok = new Token(SEP, SEMICOLON);
+            }
+            // Sep (
+            else if (input[pos] == '(') {
+                pos++;
+                tok = new Token(SEP, LEFT_PARENTHESE);
+            }
+            // Sep )
+            else if (input[pos] == ')') {
+                pos++;
+                tok = new Token(SEP, RIGHT_PARENTHESE);
+            }
+            // Sep {
+            else if (input[pos] == '{') {
+                pos++;
+                tok = new Token(SEP, LEFT_BRACE);
+            }
+            // Sep }
+            else if (input[pos] == '}') {
+                pos++;
+                tok = new Token(SEP, RIGHT_BRACE);
+            }
+            // Sep ]
+            else if (input[pos] == ']') {
+                pos++;
+                tok = new Token(SEP, RIGHT_BRACE);
+            }
+            // Sep [
+            else if (input[pos] == '[') {
+                pos++;
+                tok = new Token(SEP, LEFT_BRACKET);
+            }
+            // Sep ,
+            else if (input[pos] == ',') {
+                pos++;
+                tok = new Token(SEP, COMMA_LITERAL);
+            }
+            // Sep :
+            else if (input[pos] == ':') {
+                pos++;
+                tok = new Token(SEP, COLON);
+            }
+            // Sep .
+            else if (input[pos] == '.') {
+                pos++;
+                tok = new Token(SEP, DOT);
+            }
+            // Número inteiro
+            else if (isdigit(input[pos])) {
+                pos++;
                 while (isdigit(input[pos]))
                     pos++;
                 tok = new Token(INTEGER_LITERAL);
-                return tok;
-                break;
-        }    
+            }
+            // ID
+            else if (isalpha(input[pos]) || input[pos] == '_') {
+                while (isalpha(input[pos]) || isdigit(input[pos]) || input[pos] == '_')
+                    pos++;
+                tok = new Token(ID);
+            }
+            else if (input[pos] == '\0')
+                tok = new Token(END_OF_FILE);
+            return tok;
+        case 2:
+            // Comentário /* qualquer coisa até encontrar *
+            while (input[pos] != '*')
+                pos++;
+            //ao encontrar * vai para o estado 3
+            state = 3;
+        case 3:
+            //se encontrar / encerra o comentário
+            if (input[pos] == '/') {
+                pos++;
+                // vai para o estado um para encontrar o token
+                state = 1;
+            }
+            else //se não encontrar / volta a procurar *
+                state = 2;
+        }
     }
 }
-void Scanner::lexicalError(string msg)
-{
-    cout << "Linha "<< line << ": " << msg << endl;
-    
-    exit(EXIT_FAILURE);
-}
+    void Scanner::lexicalError(string msg)
+    {
+        cout << "Linha " << line << ": " << msg << endl;
+
+        exit(EXIT_FAILURE);
+    }
